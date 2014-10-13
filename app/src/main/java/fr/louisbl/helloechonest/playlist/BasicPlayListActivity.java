4b4f093d7f572485e7ef653afeffda2c64fa5c49
louisbl <louis@beltramo.me>
2014-10-13 21:39:54 +0200

    Use SongFragment for BasicPlayListActivity

--- app/src/main/java/fr/louisbl/helloechonest/playlist/BasicPlayListActivity.java
+++ app/src/main/java/fr/louisbl/helloechonest/playlist/BasicPlayListActivity.java
@@ -1,15 +1,9 @@
 package fr.louisbl.helloechonest.playlist;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.echonest.api.v4.EchoNestException;
 
 import fr.louisbl.helloechonest.R;
 import fr.louisbl.helloechonest.server.EchoNest;
@@ -23,7 +17,7 @@ public class BasicPlayListActivity extends Activity {
         setContentView(R.layout.activity_basic_play_list);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, SongFragment.newInstance("param 1", "param 2"))
                     .commit();
         }
     }
@@ -45,31 +39,5 @@ public class BasicPlayListActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        try {
-            EchoNest.getSimilarArtists(10, "Alec Empire");
-        } catch (EchoNestException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_basic_play_list, container, false);
-            return rootView;
-        }
     }
 }
